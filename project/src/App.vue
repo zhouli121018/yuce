4f22b9b7712cb8cd87d470288ff05ab920e52d65
zhouli <17607619682@163.com>
2019-04-10 11:12:24 +0800

    添加到主屏幕

--- project/src/App.vue
+++ project/src/App.vue
@@ -1,7 +1,21 @@
 <template>
   <div id="app">
     <a href="http://freessq.com/h/pred.apk" download v-show="false" id="download_btn">1</a>
-    <router-view />
+    <router-view v-if="!is_ios"/>
+    <div class="" v-if="is_ios" style="background:#F6F5F5;padding-top:0.4rem">
+        <!-- <title-bar title_name="添加到主屏幕" /> -->
+        <div style="background:#EFEFEF;padding:0.2rem 0.15rem;margin:0 0.3rem 0.2rem;">
+            <div style="text-align:center;font-size:0.5rem;color:#DB3030;font-weight:bold;padding:0.2rem 0;">温馨提示</div>
+            <div style="line-height:1.6;padding-left:0.2rem;font-size:0.4rem;">
+              请务必添加本页面到主屏幕，以便下次访问，点击“已添加”不再提醒
+            </div>
+            <div style="text-align:center;padding:0.4rem 0;">
+              <van-button style="width:3rem;background-color:#D4D4D4;color:#1A1A1A;font-size:0.45rem;" @click="ignore">关闭</van-button>
+              <van-button style="width:3rem;margin-left:1rem;background-color:#DB3030;color:#fff;font-size:0.45rem;" @click="addfn">已添加</van-button>
+            </div>
+        </div>
+        <img src="./assets/down_iphone.png" alt="" style="width:100%">
+    </div>
     <div class='full_sc' v-show="loading">
       <rise-loader class="custom-class" color="#8adff4" :loading="loading" :size="15" sizeUnit="px"></rise-loader>
     </div>
@@ -13,6 +27,20 @@
 import {getproperty} from '@/api/home'
 import { Dialog } from 'vant'
 export default {
+  data(){
+    return {
+      is_ios:false
+    }
+  },
+  methods:{
+    addfn(){
+      localStorage['isadd'] = true;
+      this.is_ios = false;
+    },
+    ignore(){
+      this.is_ios = false;
+    },
+  },
   created(){
     //判断是否微信或qq
     let ua = navigator.userAgent.toLowerCase();
@@ -23,7 +51,8 @@ export default {
     //判断 浏览器类型
     if (/(iPhone|iPad|iPod|iOS)/i.test(navigator.userAgent)) {
       if(!localStorage.getItem('isadd')){
-        this.$router.push('/home/ios')
+        // this.$router.push('/home/ios')
+        this.is_ios = true;
       }
     } else if (/(Android)/i.test(navigator.userAgent)) {
       if(!localStorage.getItem('isdownload')){
