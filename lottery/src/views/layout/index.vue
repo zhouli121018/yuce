55d98f388d0333cf478f176ad60dbaeaec0284dd
zhouli121018 <17607619682@163.com>
2019-09-06 22:46:55 +0800

    重新把代码给我

---
+++ lottery/src/views/layout/index.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="app_main">
+        <transition>
+                <div>
+                    <keep-alive>
+                        <router-view v-if="$route.meta.cache"></router-view>
+                    </keep-alive>
+                    <router-view v-if="!$route.meta.cache"></router-view>
+                </div>
+        </transition>
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return {
+            isLoading:false
+        }
+    },
+    methods:{
+        
+    },
+    computed: {
+        cachViews ()  {
+            console.log(this.$store.state.app.cachViews)
+            return this.$store.state.app.cachViews 
+        }
+    },
+    created(){
+        document.addEventListener('visibilitychange',()=>{
+            if(document.hidden){}else{
+                this.$router.go(0);
+            }
+        })
+    }
+    
+}
+</script>
+
+<style>
+
+</style>
